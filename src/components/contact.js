fe069f39bf5f7237406be3db4703858e469dc3cf
Jonas Thers <jonasthers1992@gmail.com>
2020-06-23 17:03:09 +0300

    Added forms

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const Contact = () => {
+  const { register, handleSubmit, errors } = useForm();
+
+  const onSubmit = () => {
+    console.log("lol");
+  };
+
   return (
     <div>
-      <h2>Contact</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          name="name"
+          ref={register({
+            required: true,
+          })}
+        />
+        {errors.name && <span>I would like to know your name</span>}
+
+        <input
+          name="email"
+          ref={register({
+            required: "We kind of need your email for this",
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+              message: "This seems like an invalid email",
+            },
+          })}
+        />
+        {errors.email && errors.email.message}
+
+        <button type="submit">Sign me up</button>
+      </form>
     </div>
   );
-}
+};
 
 export default Contact;
