7d2b56740ff320227e3d90c9cc0a7468f0c78e1d
Jonas Thers <jonas@lamia.fi>
2020-06-28 01:52:19 +0300

    404

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from "react";
 import "./styles/style.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from './components/header';
 import Home from "./components/home";
 import Contact from "./components/contact";
+import NoMatch from "./components/404";
 import Popup from "./components/popup";
 
 const App = () => {
-  return (
-    <div className="App">
-      <Router>
-        <Header />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/contact" component={Contact} />
-      </Router>
-      <Popup />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Header />
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/contact" component={Contact} />
+                    <Route component={NoMatch} />
+                </Switch>
+            </Router>
+            <Popup />
+        </div>
+    );
 };
 
 export default App;
