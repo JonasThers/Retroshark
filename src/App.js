f6dc7f511180031d5be0bc3aa95654142cf8e97d
Jonas Thers <jonas@lamia.fi>
2020-06-28 12:08:08 +0300

    Loading page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./styles/style.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from './components/header';
+import Loading from './components/loading';
 import Home from "./components/home";
 import Contact from "./components/contact";
 import NoMatch from "./components/404";
@@ -13,7 +14,8 @@ const App = () => {
             <Router>
                 <Header />
                 <Switch>
-                    <Route exact path="/" component={Home} />
+                    <Route exact path="/" component={Loading} />
+                    <Route exact path="/home" component={Home} />
                     <Route exact path="/contact" component={Contact} />
                     <Route component={NoMatch} />
                 </Switch>
---
+++ src/components/loading.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import { useHistory } from "react-router-dom";
+
+const Loading = () => {
+
+    let history = useHistory();
+
+    const [dot, setDot] = useState("");
+
+    useEffect(() => {
+        setTimeout(() => {
+            setDot(dot + ".");
+        }, 500);
+    }, [dot]);
+
+    useEffect(() => {
+        setTimeout(() => {
+            history.push("/home");
+        }, 2000);
+    }, []);
+
+    return (
+        <div className="loading">
+            <div className="loading__title">Loading{dot}</div>
+        </div>
+    );
+}
+
+export default Loading;
