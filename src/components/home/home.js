81964259528f3a769fd5502531b6dcfad2afe271
Jonas Thers <jonas@lamia.fi>
2020-06-30 19:15:42 +0300

    Stuff for homepage

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import TextBlock from "./textblock";
 
 const Home = () => {
   return (
     <div>
       <h2>Home</h2>
-      <Link to="./contact"><button>Let's get in touch</button></Link>
+        <TextBlock
+            content='Just bought a new Dreamcast, but you dont know what great games to get for it?'
+        />
+        <TextBlock
+            content='Want to buy a Turbografx-16, but afraid to get ripped off by people selling it for overprice?'
+        />
+        <TextBlock
+            content='Want to sell your old Super Nintendo, but dont know how much you can allow to charge for it?'
+        />
+      <Link to="./contact">
+          <button>Let's get in touch</button>
+      </Link>
     </div>
   );
 }
---
+++ src/components/home/textblock.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TextBlock = (props) => {
+    return (
+        <div>
+            {props.content}
+        </div>
+    )
+}
+
+export default TextBlock;
