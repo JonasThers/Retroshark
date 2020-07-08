05be9db905a1a59366427364450394342a29e39a
Jonas Thers <jonasthers1992@gmail.com>
2020-07-08 13:56:09 +0300

    Testing

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -30,7 +30,7 @@ const About = () => {
           return (
             <Case key={index} title={case.title} description={case.description} />
           );
-        })}
+        })};
     </div>
   );
 };
