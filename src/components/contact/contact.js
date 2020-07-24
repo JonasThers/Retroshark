c9f396265582d7d732fce9c5ad0161fab3b4b238
Jonas Thers <jonasthers1992@gmail.com>
2020-07-24 17:15:59 +0300

    Contact form fixes

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { useForm } from "react-hook-form";
+import React, { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
 
 const Contact = () => {
   const { register, handleSubmit, errors } = useForm();
@@ -7,7 +7,7 @@ const Contact = () => {
   const [sent, setSent] = useState(false);
 
   useEffect(() => {
-    document.title = "RetroShark | Contact";
+    document.title = 'RetroShark | Contact';
   }, []);
 
   const onSubmit = () => {
@@ -15,49 +15,55 @@ const Contact = () => {
   };
 
   return (
-    <div className="container">
+    <div className='container'>
       <div>
         In need of my help? Or would you just like a discussion with a
         like-minded geek?
       </div>
-      <div>Then let"s get in touch!</div>
+      <div>Then let's get in touch!</div>
 
       <form className='contact-form' onSubmit={handleSubmit(onSubmit)}>
-        <div className="inputs">
-          <input
-            name="name"
-            ref={register({
-              required: true,
-            })}
-          />
-          {errors.name && <span>I would like to know your name</span>}
-
-          <input
-            name="email"
-            ref={register({
-              required: "We kind of need your email for this",
-              pattern: {
-                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                message: "This seems like an invalid email",
-              },
-            })}
-          />
-          {errors.email && errors.email.message}
+        <div className='inputs'>
+          <div className='field'>
+            <input
+              name='name'
+              placeholder='Your name'
+              ref={register({
+                required: true,
+              })}
+            />
+            {errors.name && <span>I would like to know your name</span>}
+          </div>
+          <div className='field'>
+            <input
+              name='email'
+              placeholder='Your mail'
+              ref={register({
+                required: 'We kind of need your email for this',
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                  message: 'This seems like an invalid email',
+                },
+              })}
+            />
+            {errors.email && <span>{errors.email.message}</span>}
+          </div>
         </div>
         <textarea
-          name="message"
+          name='message'
+          placeholder='Your message'
           ref={register({
             required: true,
           })}
-          rows="7"
+          rows='7'
         />
-        {errors.message && <span>Don"t you have anything to say?</span>}
+        {errors.message && <span>Don't you have anything to say?</span>}
 
-        <button type="submit">Send!</button>
+        <button type='submit'>Send!</button>
       </form>
       {sent && (
         <div>
-          Your message has hereby been sent! We"ll get back to you as soon as
+          Your message has hereby been sent! We'll get back to you as soon as
           possible!
         </div>
       )}
