98d22881b8a8c9416a070e4ee2b1ad73afc70824
Jonas Thers <jonasthers1992@gmail.com>
2020-07-25 12:09:51 +0300

    Header stuff

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,16 +1,30 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
 
 const Header = () => {
-    return (
-        <div>
-            <div>Retroshark</div>
-            <div>Retro Gaming Consultancy</div>
-            <Link to="./home">Home</Link>
-            <Link to="./about">About</Link>
-            <Link to="./contact">Contact</Link>
-        </div>
-    );
-}
+  return (
+    <div>
+      <Navbar collapseOnSelect expand='lg'>
+        <Navbar.Brand>RetroShark</Navbar.Brand>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav>
+            <Nav.Link>
+              <Link to='./home'>Home</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to='./about'>About</Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to='./contact'>Contact</Link>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <div className='header-brand'>Retro Gaming Consulting</div>
+    </div>
+  );
+};
 
 export default Header;
