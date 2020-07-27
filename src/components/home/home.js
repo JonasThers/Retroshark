76864816046a877cad8146fa12c13ac81b567def
Jonas Thers <jonasthers1992@gmail.com>
2020-07-27 20:58:00 +0300

    More stuff for front page

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,34 +1,46 @@
-import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
-import TextBlock from "./textblock";
-import dreamcast from "../../imgs/dreamcast.jpg";
-import snes from "../../imgs/snes.jpg";
-import turbografx from "../../imgs/turbografx.jpg"
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import TextBlock from './textblock';
+import dreamcast from '../../imgs/dreamcast.jpg';
+import snes from '../../imgs/snes.jpg';
+import turbografx from '../../imgs/turbografx.jpg';
 
 const Home = () => {
   useEffect(() => {
-    document.title = "RetroShark | Home";
+    document.title = 'RetroShark | Home';
   }, []);
 
   return (
-    <div className="container">
-      <h2>Home</h2>
-      <div className="text-blocks">
+    <div className='container'>
+      <div className='intro'>
+        <div className='intro__text'>
+          Get the help you need when it comes to good old gaming
+        </div>
+        <div class='intro__buttons'>
+          <Link to='./about'>
+            <button>Cases</button>
+          </Link>
+          <Link to='./contact'>
+            <button>Contact me!</button>
+          </Link>
+        </div>
+      </div>
+      <div className='text-blocks'>
         <TextBlock
           imgUrl={dreamcast}
-          content="Just bought a new Dreamcast, but you dont know what great games to get for it?"
+          content='Just bought a new Dreamcast, but you dont know what great games to get for it?'
         />
         <TextBlock
           imgUrl={turbografx}
-          content="Want to buy a Turbografx-16, but afraid to get ripped off by people selling it for overprice?"
+          content='Want to buy a Turbografx-16, but afraid to get ripped off by people selling it for overprice?'
         />
         <TextBlock
           imgUrl={snes}
-          content="Want to sell your old Super Nintendo, but dont know how much you can allow to charge for it?"
+          content='Want to sell your old Super Nintendo, but dont know how much you can allow to charge for it?'
         />
       </div>
-      <Link to="./contact">
-        <button>Let"s get in touch</button>
+      <Link to='./contact'>
+        <button>Lets get in touch</button>
       </Link>
     </div>
   );
