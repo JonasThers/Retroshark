ea6aa1205e62e7c35a3ace3961998223f1b48e47
Jonas Thers <jonasthers1992@gmail.com>
2020-08-03 22:36:06 +0300

    Updates

---
+++ precache-manifest.b34903817ac00d4442b963bfe87585a2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e981adfda33112f77dae94e4298cfcc",
+    "url": "/retroshark/index.html"
+  },
+  {
+    "revision": "28831f817fefbfa77651",
+    "url": "/retroshark/static/css/main.3dd00cf6.chunk.css"
+  },
+  {
+    "revision": "c7d1097652a60c4d79e9",
+    "url": "/retroshark/static/js/2.ccdd39a4.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/retroshark/static/js/2.ccdd39a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28831f817fefbfa77651",
+    "url": "/retroshark/static/js/main.12b1f6f4.chunk.js"
+  },
+  {
+    "revision": "7a72abf6507c4e4d6a27",
+    "url": "/retroshark/static/js/runtime-main.9c513583.js"
+  },
+  {
+    "revision": "32612b25dd8a545888ed9f7b3556e41b",
+    "url": "/retroshark/static/media/dreamcast.5925a7a2.jpg"
+  },
+  {
+    "revision": "3e3589a17703b1bddd30a26ef5a4f0cd",
+    "url": "/retroshark/static/media/snes.66d46131.jpg"
+  },
+  {
+    "revision": "771ce7bfb7ce465c3e71529f6554e2fe",
+    "url": "/retroshark/static/media/turbografx.49b55477.jpg"
+  }
+]);
