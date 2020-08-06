c8c25ac249c950772bf7f46b23166a60e322505b
Jonas Thers <jonasthers1992@gmail.com>
2020-08-06 17:28:41 +0300

    Fixed navbar

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
 
 const Header = () => {
+  const [expanded, setExpanded] = useState(false);
+
   return (
     <div>
-      <Navbar collapseOnSelect expand='lg'>
+      <Navbar collapseOnSelect expand='lg' expanded={expanded}>
         <Navbar.Brand>
           <Link to='./home'>RetroShark</Link>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Toggle
+          aria-controls='responsive-navbar-nav'
+          onClick={() => setExpanded(expanded ? false : 'expanded')}
+        />
         <Navbar.Collapse id='responsive-navbar-nav'>
           <Nav>
-            <Nav.Link>
+            <Nav.Link
+              onClick={() => setExpanded(expanded ? false : 'expanded')}
+            >
               <Link to='./home'>Home</Link>
             </Nav.Link>
-            <Nav.Link>
+            <Nav.Link
+              onClick={() => setExpanded(expanded ? false : 'expanded')}
+            >
               <Link to='./about'>About</Link>
             </Nav.Link>
-            <Nav.Link>
+            <Nav.Link
+              onClick={() => setExpanded(expanded ? false : 'expanded')}
+            >
               <Link to='./contact'>Contact</Link>
             </Nav.Link>
           </Nav>
